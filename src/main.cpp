0e934985345f8a474433646c3d5bcbec4e2ab03e
Filip Rak <filip.wp.pl2014@wp.pl>
2026-01-26 17:31:54 +0100

    add launch settings for linux

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,14 @@ using namespace cc;
 auto main( int argc, char** argv ) -> int
 {
 	const cli::Options options = cli::parse( argc, argv );
+
 	app::App app( options );
 	if ( auto error = app.init(); error )
 	{
 		std::println( "Unable to initialize the app:\n{}", *error );
 		return -1;
 	}
+
 	app.run();
 	return 0;
 }
